de1355deba1216dfb9aa8bd0cd6448b1e8f12e8b
Russell Anderson <russell.anderson@homes.com>
2016-01-06 17:35:24 -0500

    coveralls is getting tiresome...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ require 'coveralls'
 
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
-  add_filter 'spec/fixtures/modules/'
+  add_filter 'spec/fixtures/modules/stdlib/'
+  add_filter 'spec/fixtures/modules/apt/'
 end
 
 # Coveralls.wear!
